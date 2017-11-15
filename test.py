96c8cdb424fa12ad0ca06915382b984347bb255b
Erik Frojdh <erik.frojdh@gmail.com>
2017-11-15 19:23:17 +0100

    new test

--- test.py
+++ test.py
@@ -5,12 +5,17 @@ Created on Wed Nov 15 19:11:10 2017
 
 @author: l_frojdh
 """
-
+import sys
 import _example
-print(help(_example.add))
+
 
 a = 5
 b = 7
 
 res = _example.add(a,b)
 print(res)
+if res == 12:
+    print("Good")
+    sys.exit(0)
+else:
+    sys.exit(1)
