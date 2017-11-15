1f6905199aa90e4bd47432f2ff6669a710c7e868
Erik Frojdh <erik.frojdh@gmail.com>
2017-11-15 19:15:15 +0100

    added test

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 19:11:10 2017
+
+@author: l_frojdh
+"""
+
+import _example
+print(help(_example.add))
+
+a = 5
+b = 7
+
+res = _example.add(a,b)
+print(res)
