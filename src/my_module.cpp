383c52610fcae054aac5602833bf9363cb5e3a7c
Erik Frojdh <erik.frojdh@gmail.com>
2018-09-22 18:21:00 +0200

    cleanup

--- src/my_module.cpp
+++ src/my_module.cpp
@@ -1,24 +1,20 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-
 #include <Python.h>
 #include <numpy/arrayobject.h>
-#include <iostream>
-
-//Include the C++ function that we want to wrap
-#include "function.h"
+#include "function.h" //this is the function that we want to call
 
 //Docstring
 static char module_docstring[] = "Example module providing a function to add two numbers";
 
 PyDoc_STRVAR(
     add_doc,
-    "Add two numbers.\n\n"
+    "Add two numbers or arrays.\n\n"
     "Parameters\n"
     "----------\n"
     "a: numpy_array\n"
-    "   single number\n"
+    "   or single number\n"
     "b: numpy_array\n"
-    "   single number\n"
+    "   or single number\n"
 
     "Returns\n"
     "----------\n"
@@ -68,18 +64,20 @@ static PyObject *add(PyObject *self, PyObject *args)
     PyObject *a_array = PyArray_FROM_OTF(a_obj, NPY_DOUBLE, NPY_ARRAY_C_CONTIGUOUS);
     PyObject *b_array = PyArray_FROM_OTF(b_obj, NPY_DOUBLE, NPY_ARRAY_C_CONTIGUOUS);
 
-    //If parsing of a or b fails we throw an exception
+    //If parsing of a or b fails we throw an exception in Python
     if (a_array == NULL || b_array == NULL)
     {
         PyErr_SetString(PyExc_TypeError, "Could not convert argument to numpy array.");
         return NULL;
     }
 
-    if (PyArray_NDIM(reinterpret_cast<PyArrayObject *>(a_array)) != PyArray_NDIM(reinterpret_cast<PyArrayObject *>(b_array)))
+    //Dimensions should agree
+    if (PyArray_NDIM((PyArrayObject *)a_array) != PyArray_NDIM((PyArrayObject *)b_array))
     {
         PyErr_SetString(PyExc_ValueError, "The size of the arguments need to match.");
         return NULL;
     }
+
     auto ndim = PyArray_NDIM(reinterpret_cast<PyArrayObject *>(a_array));
     auto a_shape = PyArray_SHAPE(reinterpret_cast<PyArrayObject *>(a_array));
     auto b_shape = PyArray_SHAPE(reinterpret_cast<PyArrayObject *>(b_array));
@@ -92,26 +90,20 @@ static PyObject *add(PyObject *self, PyObject *args)
             return NULL;
         }
     }
-    
-    if (PyArray_Size(a_array) != PyArray_Size(b_array))
-    {
-        PyErr_SetString(PyExc_ValueError, "The size of the arguments need to match.");
-        return NULL;
-    }
-
-    auto size = PyArray_Size(a_array);
-
-    /* Get a pointer to the data as C-types. */
-    double *a = (double *)PyArray_DATA((PyArrayObject *)a_array);
-    double *b = (double *)PyArray_DATA((PyArrayObject *)b_array);
 
     //Create array for return values
     PyObject *result_array = PyArray_SimpleNew(ndim, a_shape, NPY_DOUBLE);
 
-    /* Get a pointer to the data as C-types. */
+    // Get a pointer to the data for our function call
+    // I don't recommend this style but lets at least make it const =)
+    const double *a = (double *)PyArray_DATA((PyArrayObject *)a_array);
+    const double *b = (double *)PyArray_DATA((PyArrayObject *)b_array);
+
+    //And a pointer to the resutls
     double *result = (double *)PyArray_DATA((PyArrayObject *)result_array);
 
-    //Now call add wih pointers
+    //Now call add wih pointers and size
+    auto size = PyArray_Size(a_array);
     add(a, b, result, size);
 
     //Clean up
